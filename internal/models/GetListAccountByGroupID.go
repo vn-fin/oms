0b2e0e9e887d5ca555f7959e573d23c72ce8fc03
ALV0612 <levuhoangan0612@gmail.com>
2025-12-17 16:47:30 +0700

    update getting cred ID by group_id and user_id and Listing user-orders

--- internal/models/GetListAccountByGroupID.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type GetListCredentialByGroup struct {
-	GroupID     string   `json:"group_id"`
-	UserID      string   `json:"user_id"`
-	Credentials []string `json:"credential_id"`
-}
